61ed04a93038960fb023f9e1a5f940aa31fca4f9
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-06-13 02:19:56 +0900

    条件を満たすテストデータが作成されるようにseedファイルを作成、実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+  user = User.find_or_create_by!(email: "user00#{n+1}@test.com")do|_user|
+  _user.password = "test1234"
+  end
+
+  50.times do |i|
+    user.articles.find_or_create_by!(title:"No.#{i+1}:user00#{n+1}の記事")do|article|
+    article.content = "No.#{i+1}:user00#{n+1}の記事の本文"
+    end
+  end
+end
